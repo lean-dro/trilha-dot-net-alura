9cc4359c59691a14107b09e96f83061725a6cf06
lean-dro <leandro.saraiva@sptech.school>
2023-08-18 11:50:36 -0300

    Uso do Padrão AAA (Arrange, Act e Assert) para a criação de testes

---
+++ Testes em dotNet/alura.estacionamento/AluraEstacionamento.Testes/PatioTestes.cs
@@ -0,0 +1,30 @@
+﻿
+using Alura.Estacionamento.Alura.Estacionamento.Modelos;
+using Alura.Estacionamento.Modelos;
+
+namespace AluraEstacionamento.Testes
+{
+    public class PatioTestes
+    {
+        [Fact]
+        public void ValidaFaturamento()
+        {
+            //Arrange
+           var estacionamento = new Patio();
+           var veiculo = new Veiculo();
+            veiculo.Proprietario="Wagner Mancini";
+            veiculo.Tipo = TipoVeiculo.Automovel;
+            veiculo.Placa = "asd-9999";
+            
+            estacionamento.RegistrarEntradaVeiculo(veiculo);
+            estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+
+            //act
+            double faturamento = estacionamento.TotalFaturado();
+
+            //assert
+            Assert.Equal(2, faturamento);
+        }
+
+    }
+}
--- Testes em dotNet/alura.estacionamento/AluraEstacionamento.Testes/VeiculoTestes.cs
+++ Testes em dotNet/alura.estacionamento/AluraEstacionamento.Testes/VeiculoTestes.cs
@@ -1,23 +1,37 @@
+using Alura.Estacionamento.Alura.Estacionamento.Modelos;
 using Alura.Estacionamento.Modelos;
 
 namespace AluraEstacionamento.Testes
 {
+    // Padrão AAA
+    // Arrange
+    // Act
+    // Assert
     public class VeiculoTestes
     {
         [Fact]
         public void TestaVeiculoAcelerar()
         {
-            var veiculo = new Veiculo();
-            veiculo.Acelerar(100);
-            Assert.Equal(1000, veiculo.VelocidadeAtual);
+            var veiculo = new Veiculo(); // Arrange - Inicia o ambiente/cenário
+            veiculo.Acelerar(100); // Act - Uso do método
+            Assert.Equal(1000, veiculo.VelocidadeAtual); // Assert - Validação
         }
 
         [Fact]
         public void TestaVeiculoFrear()
         {
-            var veiculo = new Veiculo();
-            veiculo.Frear(10);
-            Assert.Equal(-150, veiculo.VelocidadeAtual);
+            var veiculo = new Veiculo(); // Arrange - Inicia o ambiente/cenário
+            veiculo.Frear(10); // Act - Uso do método
+            Assert.Equal(-150, veiculo.VelocidadeAtual); // Assert - Validação
         }
+        [Fact]
+        public void TestaTipoVeiculo()
+        {
+            var veiculo = new Veiculo();
+            
+            Assert.Equal(TipoVeiculo.Automovel, veiculo.Tipo);
+
+        }
+
     }
 }
