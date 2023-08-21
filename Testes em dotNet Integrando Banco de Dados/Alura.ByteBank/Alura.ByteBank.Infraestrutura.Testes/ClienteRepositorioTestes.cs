3b48309de5e14114d2486009cd9fda8e544eadc4
lean-dro <leandro.saraiva@sptech.school>
2023-08-21 15:40:39 -0300

    Uso de migrations para inserir dados no banco

---
+++ Testes em dotNet Integrando Banco de Dados/Alura.ByteBank/Alura.ByteBank.Infraestrutura.Testes/ClienteRepositorioTestes.cs
@@ -0,0 +1,28 @@
+﻿using Alura.ByteBank.Dados.Repositorio;
+using Alura.ByteBank.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Alura.ByteBank.Infraestrutura.Testes
+{
+    public class ClienteRepositorioTestes
+    {
+        [Fact]
+        public void TestaObterTodosOsClientes()
+        {
+            //Arrange
+            var repositorio = new ClienteRepositorio();
+
+            //Act 
+            List<Cliente> lista = repositorio.ObterTodos();
+
+            //Assert
+            Assert.NotNull(lista);
+            Assert.Equal(3, lista.Count);
+        }
+    }
+}
