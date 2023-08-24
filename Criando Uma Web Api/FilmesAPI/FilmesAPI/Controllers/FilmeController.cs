01a391cab614949705072a1e0608d2d2f143bf62
lean-dro <leandro.saraiva@sptech.school>
2023-08-24 14:02:10 -0300

    Criação do primeiro post

---
+++ Criando Uma Web Api/FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,16 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class FilmeController
+{
+    private static List<Filme> filmes = new List<Filme>();
+    [HttpPost]
+    public void AdicionaFilme([FromBody]Filme filme)
+    {
+        filmes.Add(filme);
+        Console.WriteLine(filme);
+    }
+}
---
+++ Criando Uma Web Api/FilmesAPI/FilmesAPI/Models/Filme.cs
@@ -0,0 +1,16 @@
+﻿namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        public string Titulo { get; set; }
+        public string Genero { get; set; }
+
+        public int Duracao { get; set; }
+
+        public override string ToString()
+        {
+            string fichaFilme = $"Filme: {this.Titulo}\nGênero: {this.Genero}\nDuração: {this.Duracao}";
+            return fichaFilme;
+        }
+    }
+}
